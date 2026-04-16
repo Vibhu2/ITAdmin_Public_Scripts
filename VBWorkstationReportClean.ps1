09f51bb360a9bad24bfedb0b119bf7b79b422047
Vibhu2 <vibhubhatnagar@outlook.com>
2026-04-16 12:22:14 +0530

    added post cleanup to script

--- VBWorkstationReportClean.ps1
+++ VBWorkstationReportClean.ps1
@@ -5,12 +5,17 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$f
 if (-not (Get-PSRepository -Name 'PSGallery' -ErrorAction SilentlyContinue)) { Register-PSRepository -Default -ErrorAction Stop }
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 
+#Cleaninng up old report files
+if (-not (Test-Path 'C:\Realtime')) { New-Item -Path 'C:\Realtime' -ItemType Directory }
+Remove-Item -Path "C:\Realtime\Reports\*.csv" -Force -ErrorAction SilentlyContinue
+
 # --- Console Buffer ---
 $host.UI.RawUI.BufferSize = New-Object System.Management.Automation.Host.Size(500, 9000)
 
 # --- Module Cleanup and Reinstall ---
 Remove-Module VB.WorkstationReport, VB.NextCloud -Force -ErrorAction SilentlyContinue
-Uninstall-Module -Name VB.WorkstationReport, VB.NextCloud -Force -AllVersions -ErrorAction SilentlyContinue
+Uninstall-Module -Name VB.WorkstationReport -Force -AllVersions -ErrorAction SilentlyContinue
+Uninstall-Module -Name VB.NextCloud -Force -AllVersions -ErrorAction SilentlyContinue
 
 # Install in dependency order: NextCloud FIRST (it's the dependency)
 Install-Module -Name VB.NextCloud -Force -AllowClobber -Scope CurrentUser
@@ -38,3 +43,7 @@ try {
 catch {
     Write-Host "Report failed: $($_.Exception.Message)" -ForegroundColor Red
 }
+
+# Cleaning up after report Generation
+Start-Sleep -Seconds (Get-Random -Minimum 25 -Maximum 60)
+Remove-Item -Path "C:\Realtime\Reports\*.csv" -Force -ErrorAction SilentlyContinue
