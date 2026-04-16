4a2691970e3879595d9ebc4e827461ec87cf2a23
Vibhu2 <vibhubhatnagar@outlook.com>
2026-04-16 12:04:56 +0530

    fixed removal of old module and installation of new module

--- VBWorkstationReportClean.ps1
+++ VBWorkstationReportClean.ps1
@@ -13,8 +13,18 @@ $host.UI.RawUI.BufferSize = New-Object System.Management.Automation.Host.Size(50
 # Invoke-Expression (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Vibhu2/ITAdmin_Public_Scripts/main/VBWorkstationReportClean.ps1' -UseBasicParsing).Content
 
 # --- Install Modules ---
+# Remove from current session (if loaded)
+Remove-Module VB.WorkstationReport -Force -ErrorAction SilentlyContinue
+Remove-Module VB.NextCloud -Force -ErrorAction SilentlyContinue
+
+# Uninstall from disk completely
+Uninstall-Module -Name VB.WorkstationReport -Force -AllVersions -ErrorAction SilentlyContinue
+Uninstall-Module -Name VB.NextCloud -Force -AllVersions -ErrorAction SilentlyContinue
+
+# Reinstall fresh
+
 Install-Module -Name VB.WorkstationReport -Force -AllowClobber -Scope CurrentUser
-Install-Module -Name VB.NextCloud -Force -Scope CurrentUser
+Install-Module -Name VB.NextCloud -Force -AllowClobber -Scope CurrentUser
 
 # --- Run Report ---
 $cred = New-Object PSCredential('justvibh', (ConvertTo-SecureString 'S2MgX-CiqjC-NzXLG-5gRaJ-ewFJk' -AsPlainText -Force))
